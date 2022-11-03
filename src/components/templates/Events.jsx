90d065577147ceec3c80b9760955c8c37463e7ce
hoon2 <sanghoon.engineer@gmail.com>
2022-11-03 15:32:13 +0900

    Events에서 EventCard 추출

--- src/components/templates/Events.jsx
+++ src/components/templates/Events.jsx
@@ -2,6 +2,7 @@ import styled, { keyframes } from "styled-components";
 import * as colors from "@styles/colors";
 import SectionLayout from "@components/molecules/SectionLayout";
 import HideScrollX from "@components/molecules/HideScrollX";
+import EventCard from "@components/molecules/EventCard";
 
 const EventsTopWrapper = styled.div`
   display: flex;
@@ -40,57 +41,6 @@ const ShowAllText = styled.span`
   color: ${colors.textSecondary};
 `;
 
-const EventCardWrapper = styled.div`
-  width: 320px;
-  height: 200px;
-  border-radius: 14px;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  background: ${colors.cardGradient};
-
-  flex-shrink: 0;
-  overflow: hidden;
-`;
-
-const EventTitles = styled.div`
-  margin-top: 16px;
-`;
-
-const CountDownWrapper = styled.div`
-  display: flex;
-  gap: 16px;
-  justify-content: center;
-  align-items: center;
-`;
-const EachCountWrapper = styled.div`
-  display: flex;
-`;
-const CountText = styled.div`
-  font-family: MarkPro-Heavy;
-  font-size: 36px;
-  line-height: 45.63px;
-  align-self: flex-start;
-`;
-
-const UnitText = styled.div`
-  font-size: 18px;
-  line-height: 27px;
-  align-self: flex-end;
-`;
-
-const EventSubTitle = styled.div`
-  font-weight: 600;
-  font-size: 14px;
-  color: ${colors.textSecondary};
-`;
-
-const EventTitle = styled.div`
-  margin-top: 4px;
-  font-weight: 700;
-  font-size: 16px;
-`;
-
 function Events() {
   return (
     <SectionLayout>
@@ -100,32 +50,7 @@ function Events() {
       </EventsTopWrapper>
       <HideScrollX>
         {[1, 2, 3, 4].map((num) => (
-          <div key={num}>
-            <EventCardWrapper>
-              <CountDownWrapper>
-                <EachCountWrapper>
-                  <CountText>08</CountText>
-                  <UnitText>일</UnitText>
-                </EachCountWrapper>
-                <EachCountWrapper>
-                  <CountText>08</CountText>
-                  <UnitText>일</UnitText>
-                </EachCountWrapper>
-                <EachCountWrapper>
-                  <CountText>08</CountText>
-                  <UnitText>일</UnitText>
-                </EachCountWrapper>
-                <EachCountWrapper>
-                  <CountText>08</CountText>
-                  <UnitText>일</UnitText>
-                </EachCountWrapper>
-              </CountDownWrapper>
-            </EventCardWrapper>
-            <EventTitles>
-              <EventSubTitle>Medistock</EventSubTitle>
-              <EventTitle>메디소사이어티 NFT 프로젝트</EventTitle>
-            </EventTitles>
-          </div>
+          <EventCard key={num} />
         ))}
       </HideScrollX>
     </SectionLayout>
