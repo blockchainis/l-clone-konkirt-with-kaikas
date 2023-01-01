c16aa84f9f3151eb4e6513c2ad91f4ad6beaef41
jhwandev <jhwan.dev@gmail.com>
2023-01-02 01:26:14 +0900

    버튼, 아이콘 이미지 업데이트

---
+++ src/components/atoms/CloseButton.jsx
@@ -0,0 +1,18 @@
+function CloseButton() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="17"
+      viewBox="0 0 18 17"
+    >
+      <polygon
+        fill="#8E8E93"
+        points="21.443 6 14.01 13.454 6.552 6.01 5.5 7.031 5.5 7.044 12.964 14.503 5.552 21.939 6.536 23 6.552 23 13.993 15.53 21.431 22.962 22.501 21.942 22.501 21.93 15.039 14.48 22.461 7.028 21.46 6"
+        transform="translate(-5 -6)"
+      />
+    </svg>
+  );
+}
+
+export default CloseButton;
